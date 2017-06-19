79e4bb88b64a6ec2df7fdcfce7e15ec02072e18f
mikebcbc <mikebconstantino@gmail.com>
2017-06-19 13:13:40 -0400

    Added list of sources to use

--- build/app.js
+++ build/app.js
@@ -1,6 +1,25 @@
 require('../build/index.html');
-let $ = require('jquery');
 import css from './main.css';
+let $ = require('jquery');
+
+let SOURCES = [
+	'breitbart-news',
+	'bbc-news',
+	'bloomberg',
+	'buzzfeed',
+	'cnbc',
+	'cnn',
+	'daily-mail',
+	'google-news',
+	'independent',
+	'the-guardian-uk',
+	'the-huffington-post',
+	'the-new-york-times',
+	'the-wall-street-journal',
+	'time',
+	'usa-today',
+	'the-washington-post'
+];
 
 $(()=>{
 	console.log('jQuery setup!');
